c28c157a7f51e1e1e0a404c1c9c7b06928af8d76
Renan Paiva <renanpaiva.dev@gmail.com>
2025-03-18 15:40:24 -0300

    Adicionando condiição ternária

--- Escopo e Estruturas de Controle em Java/ControleFluxo/src/ResultadoEscolar.java
+++ Escopo e Estruturas de Controle em Java/ControleFluxo/src/ResultadoEscolar.java
@@ -12,6 +12,16 @@ public class ResultadoEscolar {
             System.out.println("Reprovado");
 
         }
+
+        //condição ternária - cenário 1
+        nota = 7;
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+        
+        // condição ternária - cenário 2
+        nota = 6;
+        resultado = nota >= 7 ? "Aprovado" : nota >= 5 && nota < 7 ? "Recuperação" : "Reprovado";
+        System.out.println(resultado);
             
     }
 }
