293e52d68669432a7d5efb889194f5fde64d7f1c
Renan Paiva <renanpaiva.dev@gmail.com>
2025-03-25 11:01:04 -0300

    Adicionando o enhanced for loop

--- Escopo e Estruturas de Controle em Java/EstruturasRepetição/src/ExemploForArray.java
+++ Escopo e Estruturas de Controle em Java/EstruturasRepetição/src/ExemploForArray.java
@@ -3,9 +3,13 @@ public class ExemploForArray {
     public static void main(String[] args) {
         String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
 
+
         for (int x = 0; x < alunos.length; x++) {
             System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
         }
-
+        System.out.println("");
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
     }
 }
